0ceb08d2a03959f11c69c97457794166f7de4cc9
Jose Luis Fernandez Marquez <joseluis.fernandez@unige.ch>
2015-10-26 17:55:17 +0100

    added latest comments from Rosy
    - Counter down
    - removing status
    - minor changes on design

---
+++ htdocs/js/counterDown.js
@@ -0,0 +1,38 @@
+
+var end = new Date('11/24/2015 10:1 AM');
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    if (distance < 0) {
+
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = 'EXPIRED!';
+
+        return;
+    }
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    document.getElementById('countdown').innerHTML = days + 'd ';
+    document.getElementById('countdown').innerHTML += hours + 'h ';
+    document.getElementById('countdown').innerHTML += minutes + 'm ';
+    document.getElementById('countdown').innerHTML += seconds + 's';
+}
+
+timer = setInterval(showRemaining, 1000);
+
+
+
+$(document).ready(function(){
+    
+    showRemaining();
+})
